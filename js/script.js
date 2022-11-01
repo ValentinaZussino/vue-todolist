744ced07a9454e7ca6a924f2c94bdd5987185210
ValentinaZussino <vzussino@protonmail.com>
2022-11-01 17:17:15 +0100

    aggiunta alert se input vuoto

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ createApp({
     data(){
         return{
             nuovoTodo:'',
+            inputVuoto: false,
             activeIndex: 0,
             lista:[
                 {
@@ -31,7 +32,19 @@ createApp({
     },
     methods: {
         aggiungiTodo(){
-            this.lista.unshift({text: this.nuovoTodo, done: false});
+            const alertError = document.createElement('div');
+            if(this.nuovoTodo.length > 0){
+               this.lista.unshift({text: this.nuovoTodo, done: false}); 
+               this.inputVuoto = false;
+               const alertDaRimuovere = document.querySelector('.alert-danger');
+               if(alertDaRimuovere) alertDaRimuovere.remove();
+            }else{
+               this.inputVuoto = true;
+               const aggiungiAlert = document.querySelector('.add-div');
+               alertError.className='alert alert-danger vz_alert';
+               alertError.innerHTML = 'Add a Todo';
+               aggiungiAlert.append(alertError);
+            }
             this.nuovoTodo = '';
         },
         cancellaTodo(i){
